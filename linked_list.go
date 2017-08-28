c4b3ea1a542223a8db0da6e39acc51f4ee17abfe
Anondo <aanondos@gmail.com>
2017-08-29 01:33:09 +0600

    added linked_list

--- linked_list.go
+++ linked_list.go
@@ -2,30 +2,55 @@ package main
 
 import "fmt"
 
-func main(){
-    var list linked_list
-    list.push(12)
-    list.push(11)
-    fmt.Println("Hello World")
-
-}
-
-type Stack interface{
-    push(int)
-    //pop()
-}
-
-type linked_list struct{
-    head node
-}
-type node struct{
-    next linked_list
+type Node struct{
+    next *Node
     data int
 }
+func nodeConstructor(d int , n *Node) *Node{
+    newNode := new(Node)
+    newNode.next = n
+    newNode.data = d
+    return newNode
+}
 
-func (l linked_list) push(data int){
-    var temp node
-    temp.data = data
-    temp.next = l.head
+type List struct{
+    head *Node
+}
+func listConstructor() *List{
+    newlist := new(List)
+    newlist.head = nodeConstructor(0 , nil)
+    return newlist
+}
+
+type LinkedList interface{
+    push(int)
+    showList()
+    pop() int
+}
+func (l *List) push(data int){
+    temp := nodeConstructor(data , l.head)
     l.head = temp
+}
+func (l List) showList(){
+    var current = l.head
+    for current.next != nil{
+        fmt.Println(current.data)
+        current = current.next
+    }
+}
+func (l *List) pop() int{
+    data := l.head.data
+    l.head = l.head.next
+    return data
+}
+
+func main(){
+    var ll LinkedList = listConstructor()
+    ll.push(12)
+    ll.push(11)
+    ll.push(10)
+    ll.push(9)
+    ll.showList()
+    fmt.Println("Popped Value",ll.pop())
+    ll.showList()
 }
